13fd5ad8725cf38e0ea28e73379ad99f62c3ffca
Robotics <Robotics@Robotics-PC1>
2013-02-02 13:18:53 -0500

    Added comment

--- src/org/usfirst/frc354/GHouse2013/commands/AutonomousCommand.java
+++ src/org/usfirst/frc354/GHouse2013/commands/AutonomousCommand.java
@@ -26,6 +26,7 @@ public class  AutonomousCommand extends Command {
     }
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+        //do something awesome
     }
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
